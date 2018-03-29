c13accf4533b56b579dd54f374e8c741dd9c3477
Grechka_V_A <grechkava@gmail.com>
2018-03-29 04:03:09 +0200

    Converting auth view to partial

--- project/resources/views/users/auth.blade.php
+++ project/resources/views/users/auth.blade.php
@@ -1,38 +1,39 @@
-@extends ('layout')
-
-@section('content')
-
-<div class="container mt-5">
-    <div class="row">
-        <h3 class="col-lg-12 mb-4 text-center">Please login</h3>
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <form method="POST" action="/login">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Name:</label>
-                    <input name="name" class="form-control" id="name" required>
+<div class="row">
+    <div class="col-lg-12">
+        <form method="POST" action="{{ action('UsersController@login') }}">
+            @csrf
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="form-group mb-0">
+                        <label for="name">Name:</label>
+                        <input name="name" class="form-control" id="name" required>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input name="password" type="password" class="form-control" id="pwd" required>
+                <div class="col-lg-5">
+                    <div class="form-group mb-0">
+                        <label for="pwd">Password:</label>
+                        <input name="password" type="password" class="form-control" id="pwd" required>
+                    </div>
                 </div>
-                <div class="form-group">
-                <button type="submit" class="btn btn-success">Login \ Register</button>
+                <div class="col-lg-2 d-flex align-items-end">
+                    <div class="form-group mb-0">
+                    <button type="submit" class="btn btn-success">Login \ Register</button>
+                    </div>
                 </div>
-            </form>
-            @if(count($errors))
-            <div class="mt-4">
-                <ul class="list-unstyled">
-                    @foreach($errors->all() as  $error)
-                        <li class="alert alert-danger mt-2">{{ $error }}</li>
-                    @endforeach
-                </ul>
             </div>
-            @endif
+        </form>
+    </div>
+</div>
+@if(count($errors))
+<div class="row">
+    <div class="col-lg-12">
+        <div class="mt-4">
+            <ul class="list-unstyled">
+                @foreach($errors->all() as  $error)
+                    <li class="alert alert-danger mt-2">{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </div>
-
-@endsection
+@endif
